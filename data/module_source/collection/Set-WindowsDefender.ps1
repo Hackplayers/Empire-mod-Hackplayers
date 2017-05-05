050118bd9a95d1d6d30631ad63bee0b2ae9907b6
cybervaca <cybervaca@gmail.com>
2017-05-05 13:03:12 +0200

    Update Set-WindowsDefender.ps1

--- data/module_source/collection/Set-WindowsDefender.ps1
+++ data/module_source/collection/Set-WindowsDefender.ps1
@@ -6,11 +6,11 @@ $banner = @'
  / __|/ _ \ __|___\ \ /\ / / | '_ \ / _' |/ _ \ \ /\ / / __|/ _' |/ _ \ |_ / _ \ '_ \ / _' |/ _ \ '__|
  \__ \  __/ ||_____\ V  V /| | | | | (_| | (_) \ V  V /\__ \ (_| |  __/  _|  __/ | | | (_| |  __/ |   
  |___/\___|\__|     \_/\_/ |_|_| |_|\__,_|\___/ \_/\_/ |___/\__,_|\___|_|  \___|_| |_|\__,_|\___|_|   
-`n                                                                                                      
+                                                                                                      
 '@
 
 $resultado = $banner
-$resultado += "`n                                                                   CyberVaca @ HackPlayers`n`n" 
+$resultado += "`n`n                                                                   CyberVaca @ HackPlayers`n`n" 
 
 if ($Enable -like "*True*") {Set-MpPreference -DisableIOAVProtection $False ; $resultado += "[+] Windows Defender right now is Enabled" ; return $resultado}
 if ($Enable -like "*False*") {Set-MpPreference -DisableIOAVProtection $True ;$resultado += "[+] Windows Defender right now is Enabled" ; return $resultado}
